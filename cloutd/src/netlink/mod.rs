844c2a9a7def4a4169a5518986a123deadc622b4
Gregor Reitzenstein <dequbed@paranoidlabs.org>
2018-08-12 11:05:54 +0200

    Update LICENSE information

--- cloutd/src/netlink/mod.rs
+++ cloutd/src/netlink/mod.rs
@@ -5,13 +5,4 @@
  */
 
 pub mod socket;
-pub mod recv_nhrp;
-pub mod send_nhrp;
-pub mod frame;
-pub mod protocol;
-pub mod codec;
 
-pub use self::socket::*;
-pub use self::codec::NhrpCodec;
-pub use self::frame::NhrpFramed;
-pub use self::recv_nhrp::*;
--- cloutd/src/nhrp/protocol.rs
+++ cloutd/src/nhrp/protocol.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 use nom::{be_u8, be_u16, be_u32};
 
 #[derive(Debug)]
